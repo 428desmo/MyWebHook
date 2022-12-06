3b0298c54a8f11c009f5136811fcf700fbf62e33
Shibuya <rshibuya0619@gmail.com>
2022-12-06 23:45:18 +0900

    i can delete hooks now, and setup is automated

--- WebHookReceiver/server.rb
+++ WebHookReceiver/server.rb
@@ -35,19 +35,5 @@ post '/payload', provides: :json do
 end
 
 get '/' do
-  token = `cat ~/personal_access_token.txt`.chomp
-  json = `cat ~/branch_protection_rules.json`.gsub(/(\r\n?|\n| )/,"")
-
-  url="https://api.github.com/repos/TokyoDesmo/ThirdPero/branches/main/protection"
-
-  command = "curl"\
-            " -X PUT"\
-            " -H \"Accept: application/vnd.github+json\""\
-            " -H \"Authorization: Bearer #{token}\""\
-            " -H \"X-GitHub-Api-Version: 2022-11-28\""\
-            " #{url}"\
-            " -d '#{json}'"
-
-#  print("#{command}")
-  `#{command}`
+  "running"
 end
---
+++ deleteWebHooks.rb
@@ -0,0 +1,39 @@
+require 'json'
+
+# Create a personal access token (classic)
+# and store it in ~/personal_access_token.txt
+#
+# Reference:
+#   https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+
+
+token = `cat ~/personal_access_token.txt`.chomp
+organization = "TokyoDesmo"
+
+listCmd = "curl" \
+          " -H \"Accept: application/vnd.github+json\"" \
+          " -H \"Authorization: Bearer #{token}\""\
+          " -H \"X-GitHub-Api-Version: 2022-11-28\"" \
+          " https://api.github.com/orgs/#{organization}/hooks"
+
+json = `#{listCmd}`
+
+if json && !json.empty? then
+  params = JSON.parse(json)
+  params.each{
+    |key,value|
+    url = key.dig("url")
+    if url then
+        deleteCmd = "curl" \
+                    " -X DELETE" \
+                    " -H \"Accept: application/vnd.github+json\"" \
+                    " -H \"Authorization: Bearer #{token}\""\
+                    " -H \"X-GitHub-Api-Version: 2022-11-28\"" \
+                    " #{url}"
+
+        puts deleteCmd
+        `#{deleteCmd}`
+    end
+  }
+end
+
